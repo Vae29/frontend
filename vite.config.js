2b415d47e78797e8b9f8864075351b838e8c99d4
Vae29 <valesalvalen@gmail.com>
2026-06-09 03:31:52 -0500

    prueba

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
